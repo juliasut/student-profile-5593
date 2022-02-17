ea60d07ea6d51715037ea325ba024e08eaf966fa
juliasut <juliasutygina@gmail.com>
2022-02-16 20:56:36 -0700

    Fetch students data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div>Learn React</div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      students: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.hatchways.io/assessment/students')
+      .then((responce) => responce.json())
+      .then(({ students }) => this.setState({ students: students }));
+  }
+
+  render() {
+    const { students } = this.state;
+    const handleAverageGrade = (arr) => {
+      let total = arr.reduce((acc, curr) => acc + Number(curr), 0);
+      let avg = total / arr.length;
+      return avg;
+    };
+
+    console.log(students[0]);
+    return (
+      <div>
+        {students.map(
+          ({ id, pic, firstName, lastName, email, company, skill, grades }) => (
+            <div key={id}>
+              <img src={pic} alt="student" />
+              <h2 key={id}>
+                {firstName} {lastName}
+              </h2>
+              <p>Email: {email}</p>
+              <p>Company: {company}</p>
+              <p>Skill: {skill}</p>
+              <p>Average: {handleAverageGrade(grades)}%</p>
+            </div>
+          )
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
