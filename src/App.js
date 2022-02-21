0b9605fe26888e096fe8804157574ffefd3009f5
juliasut <juliasutygina@gmail.com>
2022-02-20 21:24:17 -0700

    Create search component, import to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import { Component } from 'react';
 import './App.css';
 import CardList from './components/CardList';
+import Search from './components/search/Search';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       students: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
     fetch('https://api.hatchways.io/assessment/students')
       .then((responce) => responce.json())
-      .then(({ students }) => this.setState({ students: students }));
+      .then(({ students }) => this.setState({ students }));
   }
 
   render() {
-    const { students } = this.state;
+    const { students, searchField } = this.state;
+    const filteredStudents = students.filter(({ firstName, lastName }) =>
+      (firstName + lastName).toLowerCase().includes(searchField.toLowerCase())
+    );
 
-    console.log(students[0]);
     return (
       <div>
-        <CardList students={students} />
+        <Search
+          placeholder="Search by name"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList students={filteredStudents} />
       </div>
     );
   }
